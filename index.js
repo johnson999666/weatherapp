c6a05c2b7609121cfc2cf5788d8a9a38137ba9db
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-06-17 12:44:54 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
